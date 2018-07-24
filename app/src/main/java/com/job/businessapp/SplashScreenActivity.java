5b7d51f759d0052beb19d07718a1cb2d228c2be8
JobGetabu <getabujob@gmail.com>
2018-07-24 21:45:01 +0300

    added product view and json

--- app/src/main/java/com/job/businessapp/SplashScreenActivity.java
+++ app/src/main/java/com/job/businessapp/SplashScreenActivity.java
@@ -1,7 +1,8 @@
 package com.job.businessapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
